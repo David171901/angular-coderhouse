7c4a487dcfeb3e65a648e95e494d302bebbe504a
David Pino <91108331+David171901@users.noreply.github.com>
2023-09-23 15:36:08 -0500

    Revert "Desafío entregable N1"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ToolbarComponent } from './toolbar/toolbar.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { StudentsComponent } from './students/students.component';
-import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ToolbarComponent,
-    NavbarComponent,
-    StudentsComponent,
-    FooterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
